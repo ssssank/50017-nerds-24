7a24e81f43da8e1c3526180bf6f3f1dbba333037
Alex Litvinov <ssssank@gmail.com>
2019-03-14 22:16:43 +0300

    Add form animate and validate

--- js/script.js
+++ js/script.js
@@ -2,6 +2,32 @@ var link = document.querySelector(".write-us-link");
 var popup = document.querySelector(".popup-write-us");
 var close = popup.querySelector(".popup-close");
 var login = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=email-text]");
+var form = document.querySelector(".write-us-form");
+
+var isStorageSupport = true;
+var storageLogin = "";
+var storageEmail = "";
+
+try {
+  storageLogin = localStorage.getItem("login");
+  storageEmail = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("popup-show");
+  if (storageLogin) {
+    login.value = storageLogin;
+    email.value = storageEmail;
+    text.focus();
+  } else {
+    login.focus();
+  }
+});
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -12,6 +38,7 @@ link.addEventListener("click", function(evt) {
 close.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("popup-show");
+  popup.classList.remove("popup-error");
 });
 
 window.addEventListener("keydown", function(evt) {
@@ -19,6 +46,22 @@ window.addEventListener("keydown", function(evt) {
     evt.preventDefault();
     if (popup.classList.contains("popup-show")) {
       popup.classList.remove("popup-show");
+      popup.classList.remove("popup-error");
+    }
+  }
+});
+
+form.addEventListener("submit", function(evt) {
+  evt.preventDefault();
+  if (!login.value || !email.value || !text.value) {
+
+    popup.classList.remove("popup-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("popup-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+      localStorage.setItem("email", email.value);
     }
   }
 });
